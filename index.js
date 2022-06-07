bbc298d5bfc5460739d9809a7668d244f7a2512b
Hani <md.hani@hotmail.com>
2022-06-08 00:29:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  const response= fetch("https://anapioficeandfire.com/api/books")
+  return response.then(response => response.json())
+  .then(json => renderBooks(json))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
